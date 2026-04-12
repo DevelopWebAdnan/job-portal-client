592c6dd442302e165ba5f7079e8869ee0f94431c
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-04-12 15:50:17 +0600

    Save job application and get data using email.

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
 
 const JobApply = () => {
 
@@ -19,11 +20,31 @@ const JobApply = () => {
 
         const jobApplication = {
             job_id: id,
-            application_email: user.email,
+            applicant_email: user.email,
             linkedIn,
             github,
             resume
         }
+
+        fetch('http://localhost:5000/job-applications', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(jobApplication)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Your work has been saved",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
     }
 
 
