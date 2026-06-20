4623d8b0d2f08d9a773556e98ceeb5f24ba87c25
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-06-20 20:24:17 +0600

    Sorted job data by salary from server using sort()

--- src/hooks/useJobs.jsx
+++ src/hooks/useJobs.jsx
@@ -1,19 +1,19 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-const useJobs = () => {
+const useJobs = (sort) => {
 
     const [jobs, setJobs] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get("http://localhost:5000/jobs")
+        axios.get(`http://localhost:5000/jobs?sort=${sort}`)
             .then(res => {
                 // console.log(res.data);
                 setLoading(false);
                 setJobs(res.data);
             })
-    }, [])
+    }, [sort])
 
     return { jobs, loading };
 };
--- src/pages/AllJobs/AllJobs.jsx
+++ src/pages/AllJobs/AllJobs.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useJobs from '../../hooks/useJobs';
 import HotJobCard from '../Home/HotJobCard';
 
 const AllJobs = () => {
 
-    const { jobs, loading } = useJobs();
+    const [sort, setSort] = useState(false);
+    const { jobs, loading } = useJobs(sort);
+    // console.log(sort);
 
-    if(loading) {
+    if (loading) {
         return <h2>Jobs are loading...</h2>
     }
 
     return (
         <div>
             <h1 className="text-4xl font-bold text-center py-5">All Jobs</h1>
+            <div className="py-5 p-3 bg-base-200 w-11/12 mx-auto">
+                <button onClick={() => setSort(!sort)}
+                    className={`btn btn-neutral ${sort && "btn-success"}`}
+                >{`${sort == true ? "Sorted by salary" : "Sort By Salary"}`}</button>
+            </div>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
                 {
                     jobs.map(job => <HotJobCard key={job._id} job={job}></HotJobCard>)
